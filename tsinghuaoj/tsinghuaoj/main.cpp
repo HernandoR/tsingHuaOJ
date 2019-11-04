1fceeb2264c241c0e13dcda253157458d41eadf5
Hernando <lzrichard@icloud.com>
2019-11-05 02:19:35 +0800

    ???

---
+++ tsinghuaoj/tsinghuaoj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tsinghuaoj
+//
+//  Created by 刘朕 on 2019/11/5.
+//  Copyright © 2019 朕. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
